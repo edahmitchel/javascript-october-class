3a3ce940e24d1ee033fd3534adeed68122f62930
mitchel Perekebuna Edah-Ekubo <edahmitchel@gmail.com>
2022-10-12 16:11:02 +0100

    Add files via upload

---
+++ js-trh-october/script.js
@@ -0,0 +1,22 @@
+// let a = 5
+// let b = 3
+
+
+// a = 10
+
+// console.log(a);
+
+
+
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a/b);
+// console.log(a*b);
+
+
+const a = 20;
+const b = 3;
+const c = 1;
+
+
+
